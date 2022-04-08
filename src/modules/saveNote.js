cdaa6d9ceee19331b5be7acd9002771df653bc74
Andrii Kotliar <andrikotliar@gmail.com>
2022-04-08 21:56:46 +0300

    Add logic to save edited note

--- src/modules/saveNote.js
+++ src/modules/saveNote.js
@@ -1,12 +1,25 @@
 import updateNotesList from "./updateNotesList";
+import saveEditedNote from "./saveEditedNote";
 import renderTable from "./renderTable";
 import renderStatistic from "./renderStatistic";
 
 const saveNote = (saveButtonId, notes, modal) => {
   const saveButton = document.querySelector(saveButtonId);
+  const classList = [
+    '.note-popup__title',
+    '.note-popup__content',
+    '.note-category-input'
+  ];
 
   saveButton.addEventListener('click', () => {
-    const updatedNotes = updateNotesList(notes);
+    let updatedNotes = notes;
+    if(saveButton.hasAttribute('data-id')) {
+      const id = saveButton.dataset.id;
+      saveEditedNote(notes, id, ...classList);
+      modal.removeSaveButtonAttr('data-id');
+    } else {
+      updatedNotes = updateNotesList(notes, classList);
+    }
 
     renderTable('#notesTableBody', updatedNotes, 'note');
     renderStatistic('#statisticTableBody', 'statistic__item', updatedNotes);
