13de42decb8c46a3ac36cfe562f6789520c69e27
Andrii Kotliar <andrikotliar@gmail.com>
2022-04-08 20:35:43 +0300

    Add function calls

--- src/app.js
+++ src/app.js
@@ -1,8 +1,28 @@
 import { notes } from './modules/data/notes';
+import { Modal } from './modules/classes/Modal';
 
-import generateTable from './modules/generateTable';
+import renderTable from './modules/renderTable';
+import renderStatistic from './modules/renderStatistic';
+
 import removeNote from './modules/removeNote';
+import archiveNote from './modules/archiveNote';
+import saveNote from './modules/saveNote';
+import editNote from './modules/editNote';
+import openNoteModal from './modules/openNoteModal';
+import showArchivedNotes from './modules/showArchivedNotes';
 
-generateTable('#tableBody', notes, 'note');
+const modal = new Modal(
+  '#modal',
+  '#closeModal',
+  'modal--open'
+);
 
+renderTable('#notesTableBody', notes, 'note');
+renderStatistic('#statisticTableBody', 'statistic__item', notes);
+
+archiveNote('.note__archive', notes);
 removeNote('.note__remove', notes);
+editNote('.note__edit', notes, modal);
+saveNote('#save-note', notes, modal);
+openNoteModal('.create-note', modal);
+showArchivedNotes('#showAllArchived', notes);
