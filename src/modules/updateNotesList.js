88ec1a13154296ed15b57cb2bf1c9d88873c1174
Andrii Kotliar <andrikotliar@gmail.com>
2022-04-08 21:57:14 +0300

    Remove classList array

--- src/modules/updateNotesList.js
+++ src/modules/updateNotesList.js
@@ -1,12 +1,7 @@
 import generateNewNote from "./generateNewNote";
 import generateUnicalID from "./generateUnicalID";
 
-const updateNotesList = (notes) => {
-  const classList = [
-    '.note-popup__title',
-    '.note-popup__content',
-    '.note-category-input'
-  ];
+const updateNotesList = (notes, classList) => {
 
   const newNote = generateNewNote(...classList);
   newNote.id = generateUnicalID(notes);
